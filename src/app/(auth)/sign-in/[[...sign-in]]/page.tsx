447feef00215c6d0482c61908c363f2d7c199afe
Stiven Villarreal <stivenvillarreal@gmail.com>
2025-05-20 18:49:32 -0500

    feat(ui): mejora la presentación en la página de inicio de sesión

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -9,24 +9,24 @@ export default function Page() {
 
   return (
     <div className="relative flex min-h-screen flex-col-reverse lg:flex-row font-sans">
-      <div className="hidden w-full bg-background lg:w-1/2 lg:block border-r border-dashed relative overflow-hidden">
-        <div className="absolute inset-0 bg-gradient-to-br from-background via-background/95 to-background/90 backdrop-blur-sm"></div>
+      <div className="hidden w-full bg-background lg:w-1/2 lg:block relative overflow-hidden">
+        <div className="absolute inset-0 bg-gradient-to-br dark:from-background dark:via-background/95 dark:to-background/90 backdrop-blur-sm from-foreground/60 via-background/60 to-background/60"></div>
         <div className="relative flex items-center justify-center min-h-screen">
           <div className="flex flex-col items-center space-y-6 text-center">
-            <p className="text-6xl font-bold text-primary tracking-tight animate-fade-in">
+            <p className="text-6xl font-bold text-primary tracking-tight">
               Welcome to
             </p>
-            <p className="text-7xl font-extrabold bg-gradient-to-r from-red-600 via-red-400 to-red-200 bg-clip-text text-transparent transition-all duration-300 hover:scale-105">
+            <p className="text-7xl font-extrabold bg-gradient-to-r from-red-600 dark:via-red-400 to-red-400 dark:to-red-200 bg-clip-text text-transparent transition-all duration-300 hover:scale-105">
               Noteq0
             </p>
-            <p className="text-lg text-muted-foreground max-w-md mt-4 animate-fade-in-delay">
+            <p className="text-lg text-muted-foreground max-w-md mt-4">
               Crea, organiza y comparte notas. La forma más simple de mantener
               tus ideas organizadas.
             </p>
           </div>
         </div>
       </div>
-      <div className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4 space-y-4 lg:w-1/2 lg:p-8">
+      <div className="flex min-h-screen w-full flex-col items-center justify-center bg-foreground/10 p-4 space-y-4 lg:w-1/2 lg:p-8 dark:bg-background">
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="rounded-lg border border-border bg-background py-1.5 px-2 flex items-center justify-center">
             <span className="text-4xl font-mono text-primary font-medium">
